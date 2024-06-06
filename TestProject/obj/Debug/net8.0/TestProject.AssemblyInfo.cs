2db6e12a040466ff1652e2b0c94ae6cd269ce5c9
nadeemamdev <nadeemamdev@gmail.com>
2024-06-06 15:12:27 +0100

    Add main program code

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c22e80bcacc984a33d787b3b3da608439971e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975882138e36415679ca25fe7b54fc23aee0cb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
