676f147bec039c49063671fb7f993bad5e80c569
nadeemamdev <nadeemamdev@gmail.com>
2024-06-06 13:24:41 +0100

    Add code for main program.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,53 @@
-﻿Random dice = new();
+﻿// You'll use the Random.Next() method to simulate rolling three six-sided dice. You'll evaluate the rolled values to calculate the score. If the score is greater than an arbitrary total, then you'll display a winning message to the user. If the score is below the cutoff, you'll display a losing message to the user.
+
+// If any two dice you roll result in the same value, you get two bonus points for rolling doubles.
+// If all three dice you roll result in the same value, you get six bonus points for rolling triples.
+// If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
+
+Random dice = new();
 
 int roll1 = dice.Next(1,7);
 int roll2 = dice.Next(1,7);
 int roll3 = dice.Next(1,7);
 
+// Testing using Hard Coded values for Dice Rolls
+// roll1 = 1;
+// roll2 = 1;
+// roll3 = 3;
+
 int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} {roll3} = {total}");
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine($"You rolled triples! +6 bonus to total which is now {total + 6}!");
+    total += 6;
+} 
+else 
+{
+    Console.WriteLine($"You rolled doubles! +2 bonus to total which is now {total + 2}!");
+    total += 2; 
+} 
+}
+
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new BMW M4!");
+} 
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new Macbook Pro!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a free trip to Marbella!");
+}
+else 
+{
+    Console.WriteLine("You win a kitten!");
+}
